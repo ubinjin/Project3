ec66f4aa341ff1ec71ec70098b090d60f294aa87
tjalsrb97 <gnam011@naver.com>
2021-05-27 20:57:26 +0900

    카트 제외하고 다완료 서민규

--- routes/customer.js
+++ routes/customer.js
@@ -10,7 +10,8 @@ var pool = mysql.createPool({
     port: process.env.DB_port,
     user: process.env.DB_user,
     password: process.env.DB_password,
-    database: process.env.DB_database
+    database: process.env.DB_database,
+    multipleStatements: true
 });
 
 var upload = multer({
@@ -23,42 +24,21 @@ var upload = multer({
         }
     }),
 });
-
-router.get('/', function (req, res, next) {
+//////////////////// watch product list ////////////////////////
+router.get('/', function (req, res) {
   pool.getConnection(function (err, connection) {
       var ProductList_sql = "select * from product_info";
       connection.query(ProductList_sql, function (err, rows) {
           if (err) console.error("err : " + err);
-          // console.log("rows : " + JSON.stringify(rows));
-
           res.render('main', {title: '서우장', rows: rows});
           connection.release();
       });
   });
 });
 
-// router.get('/detail/:PID',  function(req, res, next){
-//   var Product_idx = req.params.PID;
-//   pool.getConnection(function(err, connection){
-//     var Productinfo_sql = 'SELECT * FROM product_info where PID=?';
-//     var Dealinfo_sql = 'SELECT * FROM product_info as p, deal_info as d where PID=? and p.PID = d.D_PID';
-//     var Reviewinfo_sql = 'select reg.Rname, rev.R_RID, rev.R_PID, rev.R_DID, rev.Review, rev.Star, rev.Rtime, rev.Rimage from register_info as reg join review_info as rev on reg.RID = rev.R_RID where rev.R_PID=?';
-//     connection.query(Productinfo_sql, [Product_idx], function(err, product){
-//       if(err) console.error("err : "+err);
-//       connection.query(Reviewinfo_sql, [Product_idx], function(err, review){
-//         if(err) console.error("err : "+err);
-//         connection.query(Dealinfo_sql, [Product_idx], function(err, deal){
-//           if(err) console.error("err : "+err);
-//           console.log(review);
-//           res.render('detail', {title: "상품 조회", product: product[0], reviews: review, deal: deal[0]});
-//           connection.release();
-//         });
-//       });
-//     });
-//   });
-// });
 
-router.get('/detail/:PID',  function(req, res, next){
+////////////////////////////////////////////////// watch product detail info ///////////////////////////////////
+router.get('/detail/:PID',  function(req, res){
   var Product_idx = req.params.PID;
   pool.getConnection(function(err, connection){
     var Productinfo_sql = 'SELECT * FROM product_info where PID=?';
@@ -74,53 +54,84 @@ router.get('/detail/:PID',  function(req, res, next){
     });
   });
 });
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// router.post('/detail/:PID', upload.single('image'),  function(req, res, next){
-//   var R_RID = req.body.R_RID;
-//   var R_PID = req.body.R_PID;
-//   var R_DID = req.body.R_DID;;
-//   var Review = req.body.Review;
-//   var Star = req.body.Star;
-//   var Rimage = req.file.path;
-//   var Rtime = date;
-//   console.log(R_RID);
-//   console.log(R_PID);
-//   console.log(R_DID);
-//   console.log(Review);
-//   console.log(Star);
-//   console.log(Rtime);
-
-//   var datas = [R_RID, R_PID, R_DID, Review, Star, Rtime, Rimage];
-//   pool.getConnection(function(err, connection){
-//     var InsertReview_sql = "insert into review_info(R_RID, R_PID, R_DID, Review, Star, Rtime, Rimage) values(?,?,?,?,?,?,?)";
-//     connection.query(InsertReview_sql, datas, function(err, review){
-//       if(err) console.error("err : "+err);
-//       console.log("1111111");
-//       console.log("리뷰 확인", review);
-
-//       //res.render('detail', {title: "상품 조회", product: product[0], review: review[0]});
-//       res.redirect('/customer');
-//       connection.release();
-//     });
-//   });
-// });
-
-
-
-router.get('/detail/:PID/buy', function(req, res, next){
+///////////////////////////////////////////////////////// buy product /////////////////////////////////////////////////////
+router.post('/detail/:PID/buy', upload.single('image'), function(req, res){
+  /* to deal_info table */
   var Product_idx = req.params.PID;
+  var DID = req.body.P_RID + "_" + req.body.D_PID + "_" + date;
+  var P_RID = req.body.P_RID;
+  var S_RID = req.body.S_RID;
+  var D_PID = req.body.D_PID;
+  var Dtime = date;
+  var Dquantity = req.body.Dquantity;
+  var Dstate = '1';
+  /* to register_info table(update customer's cash) */
+  var Rest_cash = req.body.Rest_cash;
+  console.log(Rest_cash);
+
+  var datas = [DID, P_RID, S_RID, D_PID, Dtime, Dquantity, Dstate, Number(Rest_cash)]; 
   pool.getConnection(function (err, connection) {
-      //if (err) console.error("커넥션 객체 얻어오기 에러 : ", err);
-      var Productinfo_sql = 'SELECT * FROM product_info where PID=?';
-      connection.query(Productinfo_sql, [Product_idx], function (err, product) {
+      var InsertdealandUpdateCash_multisql = "insert into deal_info(DID, P_RID, S_RID, D_PID, Dtime, Dquantity, Dstate) values(?,?,?,?,?,?,?);" + 
+          "update register_info set cash = ? where RID = '2016722036';";
+      connection.query(InsertdealandUpdateCash_multisql, datas, function (err, result) {
           if (err) console.error(err);
-          console.log("update에서 1개 글 조회 결과 확인 : ", product);
-          // res.render('list', {title: "글 수정", row: rows[0]});
-          res.render('buy', {title: "상품 구매", product: product[0]});
+          console.log(result);
+          res.redirect('/customer/detail/' + Product_idx);
           connection.release();
       });
   });
 });
 
+router.get('/detail/:PID/buy', function(req, res){
+  var Product_idx = req.params.PID;
+  pool.getConnection(function (err, connection) {
+      if (err) console.error("커넥션 객체 얻어오기 에러 : ", err);
+      var ProductandRegisterinfo_sql = 'select * from product_info as p, register_info as r where p.PID=? and r.RID = 2016722036';
+      connection.query(ProductandRegisterinfo_sql, [Product_idx], function (err, product) {
+          if (err) console.error(err);
+          console.log("update에서 1개 글 조회 결과 확인 : ", product);
+          res.render('buy', {title: "결제", product: product[0]});
+          connection.release();
+      });
+  });
+});
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////// write review //////////////////////////////////////////////////////
+router.post('/detail/:PID/review', upload.single('image'),  function(req, res, next){
+  var Product_idx = req.params.PID;
+  var R_RID = req.body.R_RID;
+  var R_PID = req.body.R_PID;
+  var R_DID = req.body.R_DID;;
+  var Review = req.body.Review;
+  var Star = req.body.Star;
+  var Rimage = req.file.path;
+  var Rtime = date;
+
+  var datas = [R_RID, R_PID, R_DID, Review, Star, Rtime, Rimage];
+  pool.getConnection(function(err, connection){
+    var InsertReview_sql = "insert into review_info(R_RID, R_PID, R_DID, Review, Star, Rtime, Rimage) values(?,?,?,?,?,?,?)";
+    connection.query(InsertReview_sql, datas, function(err, review){
+      if(err) console.error("err : "+err);
+      res.redirect('/customer/detail/' + Product_idx);
+      connection.release();
+    });
+  });
+});
+
+router.get('/detail/:PID/review', function(req, res){
+  var Product_idx = req.params.PID;
+  pool.getConnection(function (err, connection) {
+      if (err) console.error("커넥션 객체 얻어오기 에러 : ", err);
+      var ProductandDealinfo_sql = 'select * from product_info as p, deal_info as d where d.P_RID = 2016722036 and p.PID = d.D_PID and D_PID = ?';
+      connection.query(ProductandDealinfo_sql, [Product_idx], function (err, rows) {
+          if (err) console.error(err);
+          res.render('write_review', {title: "리뷰 작성", product_deal: rows[rows.length-1]});
+          connection.release();
+      });
+  });
+});
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 module.exports = router;
