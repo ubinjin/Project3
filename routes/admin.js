8f694673355a916838cd8617c1791a0885e72dd5
Kyankee6 <didzl1231@naver.com>
2021-05-28 19:49:02 +0900

    amin.js

--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,7 @@ var pool = mysql.createPool({
     dateStrings: process.env.DB_dateStrings
 });
 
-var thumb_nail = multer({   // 진우빈 
+var thumb_nail = multer({ // 진우빈 
     storage: multer.diskStorage({
         destination: function(req, file, cb) {
             cb(null, './join_images/');
@@ -37,17 +37,23 @@ var upload = multer({
     storage: storage
 });
 
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
     res.redirect('/admin/seller_list/1');
 });
 
-router.get('/seller_list/:page', function (req, res, next) {
-    pool.getConnection(function (err, connection) {
+router.get('/seller_list/:page', function(req, res, next) {
+    pool.getConnection(function(err, connection) {
         var sqlForSelectList = "SELECT * FROM product_info";
-        connection.query(sqlForSelectList, function (err, rows) {
+        connection.query(sqlForSelectList, function(err, rows) {
             if (err) console.error("err : " + err);
             console.log("rows : " + JSON.stringify(rows));
-            res.render('seller_list', {title: '판매자 메인페이지', rows: rows});
+            res.render('seller_list', {
+                title: '판매자 메인페이지',
+                rows: rows
+            });
+        });
+    });
+});
 
 router.get('/clients_list', function(req, res, next) {
     res.redirect('clients_list/1');
@@ -76,21 +82,21 @@ router.get('/client_list/:page', function(req, res, next) {
 });
 
 
-router.post('/seller_list', function (req, res, next) {
+router.post('/seller_list', function(req, res, next) {
     var pid = req.body.pid;
     //var deletemsg = 0;
-    pool.getConnection(function (err, connection) {
+    pool.getConnection(function(err, connection) {
         var sql = "delete from product_info where pid = ?";
         //if(deletemsg){
-            for(var i=0; i<deletearray.length; i++){
-                connection.query(sql, [pid], function (err, result) {
-                    console.log(result);
-                    if (err) console.error("글 삭제 중 에러 발생 err : ", err);
-                    if (result.affectedRows == 0)
-                        res.send("<script>alert('잘못된 요청으로 인해 값이 변경되지 않습니다.');history.back();</script>");
-                });
-                console.log("시발");
-            }
+        for (var i = 0; i < deletearray.length; i++) {
+            connection.query(sql, [pid], function(err, result) {
+                console.log(result);
+                if (err) console.error("글 삭제 중 에러 발생 err : ", err);
+                if (result.affectedRows == 0)
+                    res.send("<script>alert('잘못된 요청으로 인해 값이 변경되지 않습니다.');history.back();</script>");
+            });
+            console.log("시발");
+        }
         //}
         res.redirect('/admin/seller_list');
         connection.release();
@@ -116,12 +122,12 @@ router.post('/seller_register', thumb_nail.single('pimage'), (req, res) => {
         res.send("<script>alert('상품 이미지를 넣어주세요.');history.back();</script>");
     else pimage = req.file.filename;
     var datas = [pid, pcategory, pname, price, stock, pdesc, ptime, pimage];
-    console.log("body : "+JSON.stringify(datas));
+    console.log("body : " + JSON.stringify(datas));
     pool.getConnection(function(err, connection) {
         var sqlForInsertBoard = "insert into product_info(pid, pcategory, pname, price, stock, pdesc, ptime, pimage) values(?,?,?,?,?,?,?,?)";
         connection.query(sqlForInsertBoard, datas, function(err, rows) {
             if (err) {
-                console.log("err :",err)
+                console.log("err :", err)
                 if (err.code == "ER_DUP_ENTRY")
                     return res.send("<script>alert('이미 등록되어 있는 상품 ID입니다.');history.back();</script>");
             }
@@ -130,7 +136,7 @@ router.post('/seller_register', thumb_nail.single('pimage'), (req, res) => {
                 res.send("<script>alert('상품 등록이 완료되었습니다..');window.location='http://localhost:1001/admin/seller_list/1';window.reload(true);</script>");
             }
             connection.release();
-                  });
+        });
     });
 });
 
@@ -174,17 +180,20 @@ router.post('/client_delete', function(req, res, next) {
 });
 
 
-router.get('/seller_update/:pid', thumb_nail.single('pimage'), (req, res, next)=>{
+router.get('/seller_update/:pid', thumb_nail.single('pimage'), (req, res, next) => {
     var pid = req.params.pid;
-    pool.getConnection(function (err, connection) {
+    pool.getConnection(function(err, connection) {
         if (err) console.error("커넥션 객체 얻어오기 에러 : ", err);
         var sql = "select * from product_info where pid=?"
         console.log(pid);
-        connection.query(sql, [pid], function (err, rows) {
+        connection.query(sql, [pid], function(err, rows) {
             if (err) console.error(err);
             console.log("update에서 1개 글 조회 결과 확인 : ", rows);
-            res.render('seller_update', {title: "상품 정보 수정", row: rows[0]});
-                      connection.release();
+            res.render('seller_update', {
+                title: "상품 정보 수정",
+                row: rows[0]
+            });
+            connection.release();
         });
     });
 });
@@ -226,21 +235,21 @@ router.post('/seller_update/:pid', thumb_nail.single('pimage'), (req, res) => {
     else pimage = req.file.filename;
     var datas = [pcategory, pname, price, stock, pdesc, ptime, pimage, salerate, req.params.pid];
     console.log(req.params.pid);
-    console.log("body : "+JSON.stringify(datas));
+    console.log("body : " + JSON.stringify(datas));
     pool.getConnection(function(err, connection) {
         var sql = "update product_info set pcategory=?, pname=?, price=?, stock=?, pdesc=?, ptime=?, pimage=?, salerate=? where pid=?";
         connection.query(sql, datas, function(err, rows) {
             if (err) {
-                console.log("err :",err)
-                if (err.code == "ER_DUP_ENTRY")``
-                    return res.send("<script>alert('이미 등록되어 있는 상품 ID입니다.');history.back();</script>");
+                console.log("err :", err)
+                if (err.code == "ER_DUP_ENTRY") ``
+                return res.send("<script>alert('이미 등록되어 있는 상품 ID입니다.');history.back();</script>");
             }
             else {
                 console.log("rows : " + JSON.stringify(rows));
                 res.send("<script>alert('상품 수정이 완료되었습니다..');window.location='http://localhost:1001/admin/seller_list/1';window.reload(true);</script>");
             }
             connection.release();
-                  });
+        });
     });
 });
 
@@ -249,7 +258,7 @@ router.get('/qna_detail/:idx&:Qtime', function(req, res, next) {
     var time = req.params.Qtime;
     pool.getConnection(function(err, connection) {
         var sqlQNADetail = "select q.*,Rname FROM qna_info as q join register_info on Q_RID=RID where Q_RID=? and Qtime=?";
-        connection.query(sqlQNADetail, [idx,time], function(err, row) {
+        connection.query(sqlQNADetail, [idx, time], function(err, row) {
             if (err) console.error("err : " + err);
             console.log('QNA Detail : ', row);
             res.render('qna_detail', {
@@ -268,10 +277,10 @@ router.get('/qna_detail/:idx&:Qtime', function(req, res, next) {
 router.get('/qna_answer/:idx&:Qtime', function(req, res, next) {
     var idx = req.params.idx;
     var time = req.params.Qtime;
-    console.log("idx and time: " + idx+time)
+    console.log("idx and time: " + idx + time)
     pool.getConnection(function(err, connection) {
         var sqlQNADetail = "select q.*,Rname FROM qna_info as q join register_info on Q_RID=RID where Q_RID=? and Qtime=?";
-        connection.query(sqlQNADetail, [idx,time], function(err, row) {
+        connection.query(sqlQNADetail, [idx, time], function(err, row) {
             if (err) console.error("err : " + err);
             res.render('qna_answer', {
                 title: 'Q&A 답변',
@@ -286,11 +295,11 @@ router.post('/qna_answer', function(req, res, next) {
     var idx = req.body.Q_RID;
     var qtime = req.body.Qtime;
     var content = req.body.content;
-    console.log(idx+qtime+content)
+    console.log(idx + qtime + content)
     pool.getConnection(function(err, connection) {
         console.log("getConnection error : " + err);
         var sql = "update qna_info set Answer=? where Q_RID=? and Qtime=?";
-        connection.query(sql, [content,idx,qtime], function(err, result) {  
+        connection.query(sql, [content, idx, qtime], function(err, result) {
             if (err) {
                 console.error("err : " + err);
                 res.send("<script>alert('invalid request, Check request if FK ref problem');history.back();</script>");
@@ -313,18 +322,21 @@ router.get('/thumb_nail', function(req, res) {
 });
 
 /* GET home page. */
-router.get('/seller_read/:pid',  function(req, res, next){
+router.get('/seller_read/:pid', function(req, res, next) {
     var pid = req.params.pid;
-    pool.getConnection(function(err, connection){
-    if(err) console.error("err : "+err);
-      var Productinfo_sql = 'select * from product_info where pid=?';
-      connection.query(Productinfo_sql, [pid], function(err, row){
-          if (err) console.error("err : " + err);
-          console.log("1개 상품 결과 확인 : ", JSON.stringify(row));
-          //var saleprice = row[0].price * (1 - row[0].salerate/100);
-          res.render('seller_read', { title: "상품 조회", row: row[0] });
-          connection.release();
-      });
+    pool.getConnection(function(err, connection) {
+        if (err) console.error("err : " + err);
+        var Productinfo_sql = 'select * from product_info where pid=?';
+        connection.query(Productinfo_sql, [pid], function(err, row) {
+            if (err) console.error("err : " + err);
+            console.log("1개 상품 결과 확인 : ", JSON.stringify(row));
+            //var saleprice = row[0].price * (1 - row[0].salerate/100);
+            res.render('seller_read', {
+                title: "상품 조회",
+                row: row[0]
+            });
+            connection.release();
+        });
     });
 });
 
@@ -335,17 +347,16 @@ router.post('/seller_read/:pid', (req, res) => {
         var sql = "delete from product_info where pid=?";
         connection.query(sql, datas, function(err, rows) {
             if (err) {
-                console.log("err :",err)
+                console.log("err :", err)
             }
             else {
                 console.log("rows : " + JSON.stringify(rows));
                 res.send("<script>alert('상품 삭제가 완료되었습니다..');window.location='http://localhost:1001/admin/seller_list/1';window.reload(true);</script>");
             }
             connection.release();
-                  });
+        });
     });
 });
-
 
 
 router.post('/qna_delete', function(req, res, next) {
@@ -354,7 +365,7 @@ router.post('/qna_delete', function(req, res, next) {
     console.log("질문 삭제, Q_RID : ", JSON.stringify(idx));
     pool.getConnection(function(err, connection) {
         var sql = "delete FROM qna_info where Qtime=? and Q_RID=?";
-        connection.query(sql, [qtime,idx], function(err, result) {
+        connection.query(sql, [qtime, idx], function(err, result) {
             if (err) {
                 res.send("<script>alert('invalid request, Check request if FK ref problem');history.back();</script>");
                 console.error("err : " + err);
@@ -455,13 +466,16 @@ router.post('/notice_write', upload.single('image'), function(req, res, next) {
 });
 
 
-router.get('/seller_state/:page', thumb_nail.single('pimage'), (req, res, next)=>{
-    pool.getConnection(function (err, connection) {
+router.get('/seller_state/:page', thumb_nail.single('pimage'), (req, res, next) => {
+    pool.getConnection(function(err, connection) {
         var sql = "select * from product_info, deal_info, register_info where P_RID = RID and D_PID = PID order by Dtime desc";
-        connection.query(sql, function (err, rows) {
+        connection.query(sql, function(err, rows) {
             if (err) console.error(err);
             console.log("rows : " + JSON.stringify(rows));
-            res.render('seller_state', {title: "주문 확인", row: rows});
+            res.render('seller_state', {
+                title: "주문 확인",
+                row: rows
+            });
             connection.release();
         });
     });
@@ -520,12 +534,12 @@ router.post('/notice_update', upload.single('image'), function(req, res, next) {
 
 
 router.post('/seller_state', (req, res) => {
-    pool.getConnection(function (err, connection) {
+    pool.getConnection(function(err, connection) {
         var sql_count = "select count(DID) as c from deal_info order by Dtime desc";
         var sql_find = "select DID from deal_info order by Dtime desc";
         var sql_update = "update deal_info set Dstate=? where DID=?";
-        connection.query(sql_count, function (err, row) {
-            connection.query(sql_find, function (err, row2) {
+        connection.query(sql_count, function(err, row) {
+            connection.query(sql_find, function(err, row2) {
                 for (var i = 0; i < row[0].c; i++) {
                     console.log(req.body.Dstate[i], row2[i].DID);
                     connection.query(sql_update, [req.body.Dstate[i], row2[i].DID]);
@@ -536,7 +550,6 @@ router.post('/seller_state', (req, res) => {
         res.send("<script>alert('주문 상태 변경이 완료되었습니다.');window.location='http://localhost:1001/admin/seller_state/1';window.reload(true);</script>");
     });
 });
-
 
 
 router.post('/notice_delete', function(req, res, next) {
