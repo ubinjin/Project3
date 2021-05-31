c355fbb2c164a734cceba6fe76921a9677d9745d
tjalsrb97 <gnam011@naver.com>
2021-05-31 21:21:28 +0900

    디자인 민규

--- routes/customer.js
+++ routes/customer.js
@@ -38,7 +38,6 @@ var image_upload = multer({
   }),
 });
 
-
 router.get('/', function (req, res, next) {
   if (req.session.user == undefined) {
     req.session.user = {
@@ -50,8 +49,7 @@ router.get('/', function (req, res, next) {
     };
     pool.getConnection(function (err, connection) {
       var nonmember_sql = 'insert into register_info values(?, ?, ?, ?, ?, ?, ?, ?)';
-      connection.query(nonmember_sql, [req.session.user.id, req.session.user.name, req.session.user.pw, '-', '-', 0, '-', 0], function(err,res) {
-        console.log("비회원 넣음", res);
+      connection.query(nonmember_sql, [req.session.user.id, req.session.user.name, req.session.user.pw, '-', '-', 0, req.session.user.id, 0], function(err,res) {
       });
     });
   }
@@ -70,7 +68,8 @@ router.get('/tab', function (req, res, next) {
     };
     pool.getConnection(function (err, connection) {
       var nonmember_sql = 'insert into register_info values(?, ?, ?, ?, ?, ?, ?, ?)';
-      connection.query(nonmember_sql, [req.session.user.id, req.session.user.name, req.session.user.pw, '-', '-', 0, '-', 0]);
+      connection.query(nonmember_sql, [req.session.user.id, req.session.user.name, req.session.user.pw, '-', '-', 0, req.session.user.id, 0], function(err, res) {
+      });
     });
   }
   let user_id = req.session.user.id;
@@ -81,15 +80,52 @@ router.get('/tab', function (req, res, next) {
       "SELECT * FROM product_info ORDER BY Salerate desc;" +
       "SELECT * FROM product_info ORDER BY Recommend desc;" +
       "SELECT * FROM product_info as p left join (SELECT count(*) as star_sum, R_PID FROM review_info GROUP BY R_PID) as r on R_PID = PID ORDER BY star_sum desc;" +
-      "SELECT p.PID as PID, r.rec_RID as RID FROM product_info as p join (SELECT * FROM recommend_info WHERE rec_RID = ?) as r on p.PID = r.rec_PID;";
-    connection.query(ProductList_sql, [user_id], function (err, rows) {
+      "SELECT p.PID as PID, r.rec_RID as RID FROM product_info as p join (SELECT * FROM recommend_info WHERE rec_RID = ?) as r on p.PID = r.rec_PID;" + 
+      "SELECT * FROM register_info WHERE RID=?;";
+    connection.query(ProductList_sql, [user_id, user_id], function (err, rows) {
       if (err) console.error("err : " + err);
+      console
       // console.log("rows : " + JSON.stringify(rows))
       res.render('main', {
         title: '당골찬',
         page: page,
         rows: rows,
-        header
+      });
+      connection.release();
+    });
+  });
+});
+
+router.get('/tab/:page', function (req, res, next) {
+  var page = req.params.page;
+  var user_id = req.session.user.id;
+  var where = "";
+  if (page == 0) {
+    where = "";
+  } else if (page == 1) {
+    where = " where Pcategory='새우장' ";
+  } else if (page == 2) {
+    where = " where Pcategory='게장' ";
+  } else if (page == 3) {
+    where = " where Pcategory='계란장' ";
+  } else if (page == 4) {
+    where = " where Pcategory='김치' ";
+  } else
+    where = " where Pname Like '%" + page + "%' ";
+  pool.getConnection(function (err, connection) {
+    var ProductList_sql = "SELECT * FROM product_info as p join (SELECT SUM(Dquantity) as sum, D_PID FROM deal_info, product_info GROUP BY D_PID) as d on p.PID = d.D_PID" + where + "ORDER BY sum desc;" +
+      "SELECT * FROM product_info" + where + "ORDER BY Salerate desc;" +
+      "SELECT * FROM product_info" + where + "ORDER BY Recommend desc;" +
+      "SELECT * FROM product_info as p left join (SELECT count(*) as star_sum, R_PID FROM review_info GROUP BY R_PID) as r on R_PID = PID" + where + "ORDER BY star_sum desc;" +
+      "SELECT p.PID as PID, r.rec_RID as RID FROM product_info as p join (SELECT * FROM recommend_info WHERE rec_RID = ?) as r on p.PID = r.rec_PID;" + 
+      "SELECT * FROM register_info WHERE RID=?;";
+    connection.query(ProductList_sql, [user_id, user_id], function (err, rows) {
+      if (err) console.error("err : " + err);
+      // console.log("rows : " + JSON.stringify(rows));
+      res.render('main', {
+        title: '당골찬',
+        page: page,
+        rows: rows,
       });
       connection.release();
     });
@@ -194,6 +230,7 @@ router.post('/detail/:PID/buy', upload.single('image'), function (req, res) {
   var now = new Date();
   var user_id = req.session.user.id;
   now = date_to_str(now);
+  console.log(req.body);
   /* to deal_info table */
   var Product_idx = req.params.PID;
   var DID = req.body.P_RID + "_" + req.body.D_PID + "_" + now;
@@ -203,6 +240,8 @@ router.post('/detail/:PID/buy', upload.single('image'), function (req, res) {
   var Dtime = now;
   var Dquantity = req.body.Dquantity;
   var Dstate = '결제완료';
+  var Daddress = req.body.Daddress;
+  var DphoneNum = req.body.DphoneNum;
   /* to register_info table(update customer's cash) */
   var Rest_cash = Number(req.body.Cash) - Number(req.body.Price) * Number(req.body.Dquantity);
   var Rest_stock = Number(req.body.Stock) - Number(req.body.Dquantity);
@@ -211,12 +250,12 @@ router.post('/detail/:PID/buy', upload.single('image'), function (req, res) {
   console.log(Rest_stock);
   //console.log(Rest_cash);
 
-  var datas = [DID, P_RID, S_RID, D_PID, Dtime, Dquantity, Dstate, Number(Rest_cash), user_id, Number(Rest_stock), D_PID];
+  var datas = [DID, P_RID, S_RID, D_PID, Dtime, Dquantity, Dstate, Daddress, DphoneNum, Number(Rest_cash), user_id, Number(Rest_stock), D_PID];
   if(Rest_cash >= 0){
     if(Dquantity > 0){
       if(Rest_stock >= 0){
         pool.getConnection(function (err, connection) {
-          var InsertdealandUpdateCash_multisql = "insert into deal_info(DID, P_RID, S_RID, D_PID, Dtime, Dquantity, Dstate) values(?,?,?,?,?,?,?);" +
+          var InsertdealandUpdateCash_multisql = "insert into deal_info(DID, P_RID, S_RID, D_PID, Dtime, Dquantity, Dstate, Daddress, Dphone) values(?,?,?,?,?,?,?,?,?);" +
             "update register_info set cash = ? where RID = ?;" + 
             "update product_info set stock = ? where PID = ?;";
           connection.query(InsertdealandUpdateCash_multisql, datas, function (err, result) {
@@ -303,9 +342,12 @@ router.get('/detail/:PID/review', function (req, res) {
     var ProductandDealinfo_sql = 'select * from product_info as p, deal_info as d where d.P_RID = ? and p.PID = d.D_PID and D_PID = ?';
     connection.query(ProductandDealinfo_sql, [user_id, Product_idx], function (err, rows) {
       if (err) console.error(err);
+      console.log(user_id);
+      console.log(rows);
       res.render('write_review', {
         title: "리뷰 작성",
-        product_deal: rows[rows.length - 1]
+        product_deal: rows[rows.length - 1],
+        user_id: user_id
       });
       connection.release();
     });
